f2480dedea8ccc1af8cba6fa97727ed5a3620cd7
sheltonsuen <sheltonsuen@gmail.com>
2025-06-13 11:04:57 +0800

    use hard code config instead

--- easyask/tools/chart.py
+++ easyask/tools/chart.py
@@ -1,10 +1,10 @@
 from typing import Any, List, Type, Dict
 
 from easyask.infras.chart.chart import Chart
-from easyask.infras.chart.qwen_echarts import QwenEcharts
+from easyask.infras.chart.echarts import Echarts
 
 
-def get_chart_options(dataset: List[List[Any]], dimensions: List[str], generator_cls: Type[Chart] = QwenEcharts,
+def get_chart_options(dataset: List[List[Any]], dimensions: List[str], generator_cls: Type[Chart] = Echarts,
                       config: Dict = None):
     if config is None:
         config = {}
